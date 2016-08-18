e488c36fb8a0d8607ad4dfc2ebeb64c6e22d33d0
Lemuel Reyes <reyeslemuel@hotmail.com>
2016-08-18 01:17:09 -0700

    fix navbarcomponent syntax error

--- app/components/navbar/navbar.component.ts
+++ app/components/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
 @Component({
   moduleId: module.id,
   selector: 'navbar',
-  templateUrl: 'navbar.component.html'
+  templateUrl: 'navbar.component.html',
   directives:[ROUTER_DIRECTIVES]
 })
 export class NavbarComponent {
